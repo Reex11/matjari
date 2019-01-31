0cabd65f5b01f116b2adca6cee4e139fc9f1c091
Reex-PC <reex4des@gmail.com>
2019-01-31 19:06:54 +0300

    Initial commit for first deployment.

---
+++ deploy.php
@@ -0,0 +1,43 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'alamermarket');
+
+// Project repository
+set('repository', 'git@bitbucket.org:reex11/alamermarket.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('i.reexdesign.com')
+	->user('laravel')
+    ->identityFile('~/.ssh/deployerkey')
+    ->set('deploy_path', '/var/www/alamermarket');
+//    ->set('deploy_path', '~/{{application}}');    
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+// before('deploy:symlink', 'artisan:migrate');
+
